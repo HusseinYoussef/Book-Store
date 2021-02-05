6913b416e748613caddbf3c7c7a08e420e632bf2
HusseinYoussef <husseinymousa@gmail.com>
2021-02-05 07:42:37 +0200

    Add Book Model

--- BookStore/BookStore.WebApp/Models/Book.cs
+++ BookStore/BookStore.WebApp/Models/Book.cs
@@ -6,8 +6,16 @@ namespace BookStore.WebApp.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Title { get; set; }
 
         public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+    
+        public string Language { get; set; }
+
+        public int TotalPages {get; set;}
     }
 }
