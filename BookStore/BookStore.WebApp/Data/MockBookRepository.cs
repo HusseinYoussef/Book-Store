6cb60d81a5a36c47c6beef136651452c47723567
HusseinYoussef <husseinymousa@gmail.com>
2021-02-07 02:28:03 +0200

    Update Mock Data

--- BookStore/BookStore.WebApp/Data/MockBookRepository.cs
+++ BookStore/BookStore.WebApp/Data/MockBookRepository.cs
@@ -11,11 +11,12 @@ namespace BookStore.WebApp.Data
         {
             return new List<Book>()
             {
-                new Book() {Id=1, Title="MVC", Author="Hassan", Description="This is the description of MVC Book", Category="Development", Language="Chinese", TotalPages=543},
-                new Book() {Id=2, Title="C#", Author="Hussein", Description="This is the description of C# Book", Category="Programming", Language="English", TotalPages=400},
-                new Book() {Id=3, Title="C++", Author="Negan", Description="This is the description of C++ Book", Category="Programming", Language="English", TotalPages=250},
-                new Book() {Id=4, Title="Database", Author="R Jay", Description="This is the description of Database Book", Category="Design", Language="Chinese", TotalPages=287},
-                new Book() {Id=5, Title="Azure", Author="Morgan Slizern", Description="This is the description of Azure DevOps Book", Category="Cloud", Language="Chinese", TotalPages=150}
+                new Book() {Id=1, Title="MVC", Author="Hassan", Description="This is the description of MVC Book", Categories=new List<string>{"Development", "Guide"}, Language=BookLanguage.English, TotalPages=543},
+                new Book() {Id=2, Title="C#", Author="Hussein", Description="This is the description of C# Book", Categories=new List<string>{"Guide"}, Language=BookLanguage.Arabic, TotalPages=400},
+                new Book() {Id=3, Title="C++", Author="Negan", Description="This is the description of C++ Book", Categories=new List<string>{"Guide"}, Language=BookLanguage.Chinese, TotalPages=250},
+                new Book() {Id=4, Title="Database", Author="R Jay", Description="This is the description of Database Book", Categories=new List<string>{"Development"}, Language=BookLanguage.English, TotalPages=287},
+                new Book() {Id=5, Title="Azure", Author="Morgan Slizern", Description="This is the description of Azure DevOps Book", Categories=new List<string>{"Development"}, Language=BookLanguage.German, TotalPages=150},
+                new Book() {Id=6, Title="Harry Potter", Author="Hans Zimmer", Description="This is the Harry Books Trilogy", Categories=new List<string>{"Adventure", "Fantasy"}, Language=BookLanguage.Chinese, TotalPages=723}
             };
         }
 
