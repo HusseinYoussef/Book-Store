083286bf319cbe85f5a37316b155a5a0016b5bc7
HusseinYoussef <husseinymousa@gmail.com>
2021-02-12 00:35:13 +0200

    Update Services and Configurations - Use Static Files - Add Postgres Database Service - Add AutoMapper Service - Provide Implementation for Data Repos

--- BookStore/BookStore.WebApp/Startup.cs
+++ BookStore/BookStore.WebApp/Startup.cs
@@ -2,29 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.WebApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.WebApp
 {
     public class Startup
     {
         private readonly IConfiguration _config;
+        private readonly IWebHostEnvironment _env;
 
-        public Startup(IConfiguration config)
+        public Startup(IConfiguration config, IWebHostEnvironment env)
         {
             _config = config;
+            _env = env;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<BookStoreDbContext>(options => 
+                    options.UseNpgsql(_config.GetConnectionString("BookStoreDb")));
+            services.AddScoped<IBookRepository, SqlBookRepository>();
+            services.AddScoped<ILanguageRepository, SqlLanguageRepository>();
+            services.AddScoped<ICategoryRepository, SqlCategoryRepository>();
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddAutoMapper(typeof(Startup).Assembly);
+            if(_env.IsDevelopment())
+            {
+                services.AddRazorPages().AddRazorRuntimeCompilation();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +50,8 @@ namespace BookStore.WebApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints => 
