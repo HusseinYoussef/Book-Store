a852deab6be281492c05b135a9555934243c5be6
HusseinYoussef <husseinymousa@gmail.com>
2021-02-14 01:15:54 +0200

    Add View Components - Add TopBooks ViewComponent

---
+++ BookStore/BookStore.WebApp/Components/TopBooksViewComponent.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookStore.WebApp.Data;
+using BookStore.WebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebApp.Components
+{
+    public class TopBooksViewComponent : ViewComponent
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IMapper _mapper;
+
+        public TopBooksViewComponent(IBookRepository bookRepository, IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var books = await _bookRepository.GetTopBooks(count:3);
+            return View(_mapper.Map<IEnumerable<BookViewModel>>(books));
+        }
+    }
+}
