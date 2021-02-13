6c8690cc809061785d67f76ea1bb9af87e514237
HusseinYoussef <husseinymousa@gmail.com>
2021-02-13 21:22:58 +0200

    Add GalleryViewModel

---
+++ BookStore/BookStore.WebApp/ViewModels/GalleryViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookStore.WebApp.ViewModels
+{
+    public class GalleryViewModel
+    {
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+    }
+}
