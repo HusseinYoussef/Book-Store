768f76c861cbdf6108e1af4e48aa55b9e893a696
HusseinYoussef <husseinymousa@gmail.com>
2021-03-13 16:02:21 +0200

    Update Json Config

--- BookStore/BookStore.WebApp/ViewModels/BookViewModel.cs
+++ BookStore/BookStore.WebApp/ViewModels/BookViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using BookStore.WebApp.Validations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace BookStore.WebApp.ViewModels
 {
@@ -23,40 +24,51 @@ namespace BookStore.WebApp.ViewModels
 
         [Required(ErrorMessage="Please select a language")]
         [Display(Name="Language")]
+        [Newtonsoft.Json.JsonIgnore]
         public int LanguageId { get; set; }
 
+        [JsonProperty("Language")]
         public string LanguageName { get; set; }
         
         [Required]
         [Range(100, 1000, ErrorMessage="Pages number should be in range of 100 and 1000")]
         [Display(Name="Number of Pages")]
+        [JsonProperty("Total Pages")]
         public int? TotalPages { get; set; }
 
         [Required(ErrorMessage="Please select one or more categories")]
         [Display(Name="Category")]
+        [Newtonsoft.Json.JsonIgnore]
         public IEnumerable<int> CategoryIds { get; set; }
 
+        [JsonProperty("Categories")]
         public IEnumerable<string> CategoryNames { get; set; }
 
         [Required]
         [ImageExtensions]
         [Display(Name="Choose Cover Photo")]
+        [Newtonsoft.Json.JsonIgnore]
         public IFormFile CoverPhoto { get; set; }
 
+        [JsonProperty("Cover Photo")]
         public string CoverPhotoPath { get; set; }
 
         [Required]
         [ImageExtensions]
         [Display(Name="Gallery")]
+        [Newtonsoft.Json.JsonIgnore]
         public IFormFileCollection GalleryFiles { get; set; }
 
+        [Newtonsoft.Json.JsonIgnore]
         public List<GalleryViewModel> Gallery { get; set; }
 
         [Required]
         [Display(Name="Upload book in pdf format")]
         [PdfExtension]
+        [Newtonsoft.Json.JsonIgnore]
         public IFormFile Pdf { get; set; }
 
+        [JsonProperty("Pdf")]
         public string PdfPath { get; set; }
     }
 }
