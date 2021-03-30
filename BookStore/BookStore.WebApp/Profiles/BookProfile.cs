0240696d5940ba7c045ca0dda8d9f274951be4f3
HusseinYoussef <husseinymousa@gmail.com>
2021-03-30 03:40:29 +0200

    Updated BookProfile

--- BookStore/BookStore.WebApp/Profiles/BookProfile.cs
+++ BookStore/BookStore.WebApp/Profiles/BookProfile.cs
@@ -10,8 +10,10 @@ namespace BookStore.WebApp.Profiles
     {
         public BookProfile()
         {
-            CreateMap<Book, BookViewModel>().ForMember(dest => dest.CategoryNames, opt => 
-                                    opt.MapFrom(src => src.Category.Select(c=>c.Name)));
+            CreateMap<Book, BookViewModel>().ForMember(dest => dest.CategoryNames, opt =>
+                                    opt.MapFrom(src => src.Category.Select(c => c.Name)))
+                                            .ForMember(dest => dest.CategoryIds, opt =>
+                                    opt.MapFrom(src => src.Category.Select(c => c.Id)));
             CreateMap<BookViewModel, Book>();
         }
     }
