52e3a47a60a9e0ee366303a4ff7de0cd5de2bbc7
HusseinYoussef <husseinymousa@gmail.com>
2021-03-30 03:36:35 +0200

    Added Edit and Delete Action methods - Added Get/Post Edit action methods - Added Delete action method

--- BookStore/BookStore.WebApp/Controllers/BookController.cs
+++ BookStore/BookStore.WebApp/Controllers/BookController.cs
@@ -50,17 +50,18 @@ namespace BookStore.WebApp.Controllers
         }
 
         [Route("Books/BookDetails")]
-        public async Task<ViewResult> GetBookDetails(int id)
+        public async Task<ViewResult> GetBookDetails(int id, BookStatus bookStatus = BookStatus.Default)
         {
             Book book = await _bookRepository.GetBookById(id);
             BookViewModel bookModel = _mapper.Map<BookViewModel>(book);
             bookModel.Gallery = book.BookGallery.Select(bg => _mapper.Map<GalleryViewModel>(bg)).ToList();
+            ViewBag.status = bookStatus;
             return View(bookModel);
         }
 
         [Authorize]
         [HttpGet("Books/AddBook")]
-        public ViewResult AddBook(AddBookStatus status=AddBookStatus.Default, int bookId=0)
+        public ViewResult AddBook(BookStatus status = BookStatus.Default, int bookId = 0)
         {
             ViewBag.bookId = bookId;
             ViewBag.status = status;
@@ -68,16 +69,17 @@ namespace BookStore.WebApp.Controllers
         }
 
         [Authorize]
+        [ValidateAntiForgeryToken]
         [HttpPost("Books/AddBook")]
         public async Task<IActionResult> AddBook(BookViewModel newBook)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(newBook);
             }
-            if(await _bookRepository.CheckBookName(newBook.Title))
+            if (await _bookRepository.CheckBookName(newBook.Title))
             {
-                ViewBag.status = AddBookStatus.Fail;
+                ViewBag.status = BookStatus.Fail;
                 return View(newBook);
             }
 
@@ -88,23 +90,24 @@ namespace BookStore.WebApp.Controllers
             book.PdfPath = await UploadFile("books/pdf/", newBook.Pdf);
             // Upload gallery Photos
             book.BookGallery = new List<Gallery>();
-            foreach(var file in newBook.GalleryFiles)
+            foreach (var file in newBook.GalleryFiles)
             {
-                book.BookGallery.Add(new Gallery(){
-                    Name=file.FileName,
-                    Path=await UploadFile("books/images/gallery/", file)
+                book.BookGallery.Add(new Gallery()
+                {
+                    Name = file.FileName,
+                    Path = await UploadFile("books/images/gallery/", file)
                 });
             }
             IEnumerable<Category> categories = await _categoryRepository.GetCategoriesById(newBook.CategoryIds);
             book.Category = new List<Category>();
-            foreach(Category category in categories)
+            foreach (Category category in categories)
             {
                 book.Category.Add(category);
             }
             book.CreatedAt = book.UpdatedAt = DateTime.UtcNow;
             book.UserId = _userService.GetUserId();
             int bookId = await _bookRepository.AddBook(book);
-            return RedirectToAction(nameof(AddBook), new {status=AddBookStatus.Success, bookId=bookId});
+            return RedirectToAction(nameof(AddBook), new { status = BookStatus.Success, bookId = bookId });
         }
 
         private async Task<string> UploadFile(string folder, IFormFile file)
@@ -112,14 +115,14 @@ namespace BookStore.WebApp.Controllers
             string path = folder + Guid.NewGuid().ToString() + "_" + file.FileName;
             string serverFolder = Path.Combine(_env.WebRootPath, path);
             await file.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
-            
+
             return "/" + path;
         }
 
         [HttpGet("search")]
-        public async Task<IActionResult> Search([FromQuery] string by, [FromQuery(Name="search_query")] string searchQuery)
+        public async Task<IActionResult> Search([FromQuery] string by, [FromQuery(Name = "search_query")] string searchQuery)
         {
-            if(string.IsNullOrEmpty(searchQuery))
+            if (string.IsNullOrEmpty(searchQuery))
             {
                 return RedirectToAction("Index", "Home");
             }
@@ -127,7 +130,7 @@ namespace BookStore.WebApp.Controllers
             ViewBag.by = by;
             ViewBag.searchQuery = searchQuery;
             IEnumerable<Book> books = null;
-            if(by == "title")
+            if (by == "title")
             {
                 books = await _bookRepository.SearchByTitle(searchQuery);
             }
@@ -135,7 +138,7 @@ namespace BookStore.WebApp.Controllers
             {
                 books = await _bookRepository.SearchByCategory(searchQuery);
             }
-            else if(by == "author")
+            else if (by == "author")
             {
                 books = await _bookRepository.SearchByAuthor(searchQuery);
             }
@@ -143,11 +146,76 @@ namespace BookStore.WebApp.Controllers
             return View("SearchResults", _mapper.Map<IEnumerable<BookViewModel>>(books));
         }
 
+        [Authorize]
         [HttpGet("Library")]
         public async Task<ViewResult> Library()
         {
             IEnumerable<Book> books = await _bookRepository.GetLibrary(_userService.GetUserId());
             return View(_mapper.Map<IEnumerable<BookViewModel>>(books));
+        }
+
+        [Authorize]
+        [HttpGet("Books/Edit")]
+        public async Task<IActionResult> EditBook(int id)
+        {
+            Book book = await _bookRepository.GetBookById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(_mapper.Map<BookViewModel>(book));
+        }
+
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        [HttpPost("Books/Edit")]
+        public async Task<IActionResult> EditBook(BookViewModel bookViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(bookViewModel);
+            }
+            if (await _bookRepository.CheckBookName(bookViewModel.Title, bookViewModel.Id))
+            {
+                ModelState.AddModelError("", "Book Title is already used");
+                return View(bookViewModel);
+            }
+
+            Book book = await _bookRepository.GetBookById(bookViewModel.Id);
+            _mapper.Map(bookViewModel, book);
+            book.CoverPhotoPath = await UploadFile("books/images/cover/", bookViewModel.CoverPhoto);
+            book.PdfPath = await UploadFile("books/pdf/", bookViewModel.Pdf);
+            book.BookGallery = new List<Gallery>();
+            foreach (var file in bookViewModel.GalleryFiles)
+            {
+                book.BookGallery.Add(new Gallery()
+                {
+                    Name = file.FileName,
+                    Path = await UploadFile("books/images/gallery/", file)
+                });
+            }
+            IEnumerable<Category> categories = await _categoryRepository.GetCategoriesById(bookViewModel.CategoryIds);
+            book.Category = new List<Category>();
+            foreach (Category category in categories)
+            {
+                book.Category.Add(category);
+            }
+            book.UpdatedAt = DateTime.UtcNow;
+            await _bookRepository.SaveUpdates();
+            return RedirectToAction(nameof(GetBookDetails), new {id=book.Id, bookStatus=BookStatus.Success});
+        }
+
+        [Authorize]
+        [HttpDelete("Books/Delete")]
+        public async Task<IActionResult> DeleteBook([FromQuery] int id)
+        {
+            Book book = await _bookRepository.GetBookById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            await _bookRepository.DeleteBook(book);
+            return Ok();
         }
     }
 }
